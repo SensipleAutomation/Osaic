204b96bc1a9be1376d400e38306fb6265c9d1cbf
SensipleAutomation <147045985+SensipleAutomation@users.noreply.github.com>
2024-01-06 23:35:50 +0530

    Update UIRunner.java

--- UIRunner.java
+++ UIRunner.java
@@ -1,2 +1,7 @@
-Could not write workspace metadata 'C:\Users\rpuliendran\eclipse-workspace\.metadata\.plugins\org.eclipse.core.resources\14.snap'.
-There is not enough space on the disk
+ org.openqa.selenium.WebDriverException: unknown error: failed to write prefs file
+Build info: version: '3.10.0', revision: '176b4a9', time: '2018-03-02T19:03:16.397Z'
+System info: host: 'PW1ETSQUAS004AV', ip: '10.128.17.177', os.name: 'Windows 10', os.arch: 'amd64', os.version: '10.0', java.version: '17.0.9'
+Driver info: driver.version: RemoteWebDriver
+remote stacktrace: 
+	at java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)
+	at java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:77)
